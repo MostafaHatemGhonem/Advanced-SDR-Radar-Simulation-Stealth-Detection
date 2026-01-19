d7df6c2703175025984a7241a444584c88bacccf
MostafaHatemGhonem <hatmmstfy536@gmail.com>
2026-01-19 03:09:53 +0200

    Initial project structure and global radar config

---
+++ config.py
@@ -0,0 +1,8 @@
+# SkyGuard Radar Global Configuration
+C = 3e8              # Speed of light (m/s)
+R_MAX = 150000       # Maximum Range 150 KM
+FC = 10e9            # Carrier Frequency 10GHz (X-band)
+B = 150e6            # Bandwidth 150MHz
+TC = 2e-3            # Chirp Duration 2ms
+FS = 1e6             # Initial Sampling Rate (1MHz)
+STEALTH_RCS = 0.01   # RCS for Stealth Target (m^2)
